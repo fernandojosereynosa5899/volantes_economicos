fbc18343e84a1cc450c50d0d5a1573a679a54e28
fernandojosereynosa5899 <fernando.jose.reynosa@gmail.com>
2026-09-09 21:12:52 -0600

    alinear URL canonica y schema a   volanteseconomicos.vercel.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://volanteseconomicos.net',
+  site: 'https://volanteseconomicos.vercel.app',
   server: {
     host: true,
     port: 4321,
